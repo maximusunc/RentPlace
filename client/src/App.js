010a552d32107f3c00342144e42ee7cbb60c81c5
Max Wang <maxwellswang@gmail.com>
2018-02-03 13:38:13 -0500

    testing heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import Splash from "./pages/home";
 
 const App = () =>
   <Router>
     <div>
-      <Nav />
       <Switch>
-        <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
-        <Route component={NoMatch} />
+        <Route exact path="/" component={Splash} />
+        {/* <Route component={NoMatch} /> */}
       </Switch>
     </div>
   </Router>;
---
+++ client/src/pages/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Container from "../components/container";
+
+class Splash extends Component {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <Container>
+                <h1>Hello</h1>
+            </Container>
+        );
+    };
+}
+
+export default Splash;
