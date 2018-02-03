0b6f59012973740eb5d0880652cbcb5b0d62e371
Matthew Nagy <grndprxguy@gmail.com>
2018-02-03 13:17:58 -0500

    created models

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
-  {
-    useMongoClient: true
-  }
+  process.env.MONGODB_URI || "mongodb://localhost/rentplace"
 );
 
 // Start the API server
