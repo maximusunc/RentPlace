2b497b626d07bd7c641d766c1a9156ecc27e7a94
Matthew Nagy <grndprxguy@gmail.com>
2018-02-08 21:18:44 -0500

    added routes for findByLAndlord, findByTenant

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -5,12 +5,43 @@ module.exports = {
   findAll: function(req, res) {
     db.ServiceReq
       .find({})
+      .populate('_landlord')
+      .populate('_tenant')
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
     db.ServiceReq
       .findById(req.params.id)
+      .populate('_landlord')
+      .populate('_tenant')
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  // find by property id
+  findByPropId: function (req, res) {
+    db.ServiceReq
+      .find({ '_property': req.params.id })
+      .populate('_landlord')
+      .populate('_tenant')
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  // find by landlord
+  findByLandlord: function (req, res) {
+    db.ServiceReq
+      .find({ '_landlord': req.params.id })
+      .populate('_landlord')
+      .populate('_tenant')
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  // find by tenant
+  findByTenant: function (req, res) {
+    db.ServiceReq
+      .find({ '_tenant': req.params.id })
+      .populate('_landlord')
+      .populate('_tenant')
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
