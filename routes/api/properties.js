ffcc800d0ab15b7982d339ff6741f951a1c4cbd2
Matthew Nagy <grndprxguy@gmail.com>
2018-02-08 21:28:55 -0500

    added routes for findByLAndlord, findByTenant

--- routes/api/properties.js
+++ routes/api/properties.js
@@ -6,6 +6,14 @@ router.route("/")
   .get(propertiesController.findAll)
   .post(propertiesController.create);
 
+// Matches with "/api/properties/findbylandlord/:id"
+router.route("/findbylandlord/:id")
+  .get(propertiesController.findByLandlord);
+
+// Matches with "/api/properties/findbytenant/:id"
+router.route("/findbytenant/:id")
+  .get(propertiesController.findByTenant);
+
 // Matches with "/api/properties/:id"
 router.route("/:id")
   .get(propertiesController.findById)
