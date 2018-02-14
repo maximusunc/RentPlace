dea3d763bc5f6f560d36944469b593b48ac3d77a
Max Wang <maxwellswang@gmail.com>
2018-02-14 14:54:28 -0500

    just a few more style changes

--- client/src/components/allTenants/allTenants.js
+++ client/src/components/allTenants/allTenants.js
@@ -5,7 +5,9 @@ const Tenant = (props) =>
     <Container>
         <h5>Name:  {props.name}</h5>
         <h6>Email: {props.email}</h6>
-        <button onClick={props.handleClick}>Add tenant to property</button>
+        <button onClick={props.handleClick} className="waves-effect waves-teal btn-medium"><i className="material-icons left">add</i>
+            Add tenant to property
+        </button>
     </Container>;
 
 export default Tenant;
